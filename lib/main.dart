e0cc814045a6abe9511ffbf43e7dbeb7a0d8fb17
FlutLab.io <>
2022-05-04 15:49:38 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:marquez/models/catalog.dart';
+import 'package:marquez/widgets/itemWidget.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Catalogo Marquez"),
+        ),
+        body: ListView.builder(
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: CatalogModel.items[index]);
+            }),
+      ),
+    );
+  }
+}
